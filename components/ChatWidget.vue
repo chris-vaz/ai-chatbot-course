4074163028ad0569e6a9c403763f5bb567bc0290
chris-vaz <chrisvas2001@gmail.com>
2024-05-08 14:52:35 +0300

    Completed chatbot to give the full context of the conversation

--- components/ChatWidget.vue
+++ components/ChatWidget.vue
@@ -18,17 +18,37 @@ const messages = ref<Message[]>([]);
 
 const usersTyping = ref<User[]>([]);
 
+const messagesForApi = computed(() =>
+  messages.value.map(m => ({
+    role: m.userId,
+    content: m.text
+  })))
+
 // send messages to Chat API here
 // and in the empty function below
 
-async function handleNewMessage(message: Message) {}
+async function handleNewMessage(message: Message) {
+  messages.value.push(message);
+  usersTyping.value.push(bot.value)
+  const res: any = await $fetch("/api/ai", {
+    method: "POST",
+    body: {
+      message: messagesForApi.value,
+    },
+  });
+
+  if (!res.choices[0].message?.content) return;
+
+  const msg = {
+    id: res.id,
+    userId: bot.value.id,
+    createdAt: new Date(),
+    text: res.choices[0].message?.content
+  };
+  messages.value.push(msg);
+  usersTyping.value = [];
+}
 </script>
 <template>
-  <ChatBox
-    :me="me"
-    :users="users"
-    :messages="messages"
-    @new-message="handleNewMessage"
-    :usersTyping="usersTyping"
-  />
+  <ChatBox :me="me" :users="users" :messages="messages" @new-message="handleNewMessage" :usersTyping="usersTyping" />
 </template>
